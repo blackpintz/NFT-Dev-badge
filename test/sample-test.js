a77897806779ec145dc7e24660262d7c16320ee8
blackpintz <rosewanjohi24@gmail.com>
2021-12-01 18:46:07 +0300

    add more unit tests.

--- test/sample-test.js
+++ test/sample-test.js
@@ -48,5 +48,50 @@ describe("Test All contracts", function() {
     })
   })
 
+  describe('Job contract functions', function () {
+    let nftContractAddress;
+    let title;
+    let rewardPrice;
+    let deadline;
+    let description;
+    let category;
 
+    beforeEach(() => {
+      nftContractAddress = nft.address
+      rewardPrice = ethers.utils.parseUnits('100', 'ether')
+      title = "first job"
+      deadline = "Dec 1st"
+      description = "Description of first job"
+      category = "security"
+    })
+    it('should create job item', async function () {
+      await deployedJob.createJobItem(nftContractAddress, title, rewardPrice, deadline, description, category)
+      const jobsPosted = await deployedJob.fetchJobItems()
+      assert.equal(jobsPosted.length, 1)
+    })
+
+    it('should submit job item', async function () {
+      const [_, geAddress, workerAddress] = await ethers.getSigners()
+      await deployedJob.createJobItem(nftContractAddress, title, rewardPrice, deadline, description, category)
+      await deployedJob.connect(workerAddress).createJobSubmitted(1, "github.com")
+      const jobsSubmitted = await deployedJob.fetchJobsSubmitted()
+      assert.equal(jobsSubmitted.length, 1)
+    })
+
+    it('should approve job item', async function () {
+      const [_, geAddress, workerAddress] = await ethers.getSigners()
+      await deployedJob.createJobItem(nftContractAddress, title, rewardPrice, deadline, description, category)
+      await deployedJob.connect(workerAddress).createJobSubmitted(1, "github.com")
+      await deployedJob.transferRewardsAndNft(1,nftContractAddress,1, 1, {value: rewardPrice})
+      const jobsApproved = await deployedJob.fetchJobsApproved()
+      assert.equal(jobsApproved.length, 1)
+    })
+  })
+
+  describe('MetaData contract functions', function () {
+    it('should return a hash value', async function () {
+      const hashValue = await metadata.hash(2, "github.com", '0x8462eb2fbcef5aa4861266f59ad5f47b9aa6525d767d713920fdbdfb6b0c0b78')
+      assert.equal(hashValue, '0x6adcb8d4faf4653834d8f55f779f32c3771db3832b7b3f620b087e6bad3bbe15')
+    })
+  })
 })
