e776c0efd119e8f6f3b79472e3fb62a8eaccd9b1
blackpintz <rosewanjohi24@gmail.com>
2021-12-02 03:13:31 +0300

    update webpack config.

--- next.config.js
+++ next.config.js
@@ -3,13 +3,5 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  webpack(config) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-
-    return config;
-  },
   trailingSlash: true,
 }
